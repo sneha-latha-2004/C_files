8552d26af08ac45e24317dc83f929d166ea96cbd
sneha-latha-2004 <sneha22ec052@drttit.edu.in>
2026-03-25 12:58:21 +0530

    dec

---
+++ dec_pointer.c
@@ -0,0 +1,18 @@
+ #include <stdio.h>
+
+int main() {
+    FILE *fp1, *fp2;
+    char ch;
+
+    fp1 = fopen("source.txt", "r");
+    fp2 = fopen("dest.txt", "w");
+
+    while((ch = fgetc(fp1)) != EOF) {
+        fputc(ch, fp2);
+    }
+
+    fclose(fp1);
+    fclose(fp2);
+
+    return 0;
+}
